3097bd5dc72ea90e6a9b9158f088d0c1c527a5ae
Ryan Halliwell <git@rayliwell.com>
2024-04-24 16:00:05 +0100

    feat: switch from `dream2nix` to `buildNpmPackage`

--- flake.nix
+++ flake.nix
@@ -2,31 +2,40 @@
   description = "My personal website";
 
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    inputs@{
-      self,
-      dream2nix,
-      nixpkgs,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-    in
     {
-      packages.${system}.default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
-        modules = [
-          ./default.nix
-          {
-            paths.projectRoot = ./.;
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          default = pkgs.buildNpmPackage {
+            name = "www";
+            src = ./.;
+
+            buildInputs = [ pkgs.nodejs_18 ];
+
+            npmDepsHash = "sha256-gLRr3Vru1Fot3HMllid9PITP6hJQIv+MAxNjpJblmJE=";
+
+            buildPhase = ''
+              mkdir -p $out/app
+              cp -r . $out/app
+              cd $out/app
+              npm run build
+              ln -s $out/app/node_modules/.bin $out/bin
+            '';
+          };
+        };
+      }
+    );
 }
