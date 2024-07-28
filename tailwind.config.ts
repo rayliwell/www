f3f38198a0bd5b71f1c48ba20c4c34f4e2b0c873
Ryan Halliwell <git@rayliwell.com>
2024-07-28 15:54:25 +0100

    feat: add `tailwindcss-scroller` dependency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,26 @@
 import type { Config } from 'tailwindcss'
+import typographyPlugin from '@tailwindcss/typography'
+import scrollerPlugin from 'tailwindcss-scroller'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      primary: 'rgb(var(--primary) / <alpha-value>)',
-      secondary: 'rgb(var(--secondary) / <alpha-value>)',
+    scroller: {
+      title: {
+        stopTime: 1000,
+        moveTime: 500,
+        direction: 'down',
+      },
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--primary) / <alpha-value>)',
+        secondary: 'rgb(var(--secondary) / <alpha-value>)',
+      },
+      fontFamily: {
+        title: ['"Exo 2"', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [typographyPlugin, scrollerPlugin],
 } as Config
