c8e3ad8365e678ef97ce5608bad534b3241da1a4
Ryan Halliwell <git@rayliwell.com>
2024-07-28 15:57:57 +0100

    feat: add `vite-icon-loader` dependency

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, passthroughImageService } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
+import icons from 'vite-icon-loader'
 
 export default defineConfig({
   integrations: [tailwind()],
@@ -14,6 +15,7 @@ export default defineConfig({
     assets: 'assets',
   },
   vite: {
+    plugins: [icons({ cssTemplate: (url: string) => `--url: url(${url});` })],
     server: {
       watch: {
         ignored: ['**/node_modules/**', '**/.git/**', '**/.direnv/**'],
