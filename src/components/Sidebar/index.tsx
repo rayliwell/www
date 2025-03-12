675bf21a381704900202d821a5d3ddf9c1d6b763
Ryan Halliwell <git@rayliwell.com>
2025-03-12 16:31:28 +0000

    feat: update sidebar style

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,22 +4,23 @@ import IconButton from '@components/IconButton'
 
 export const id = 'sidebarMenu'
 
-export default function Sidebar() {
-  const links = [
-    { displayName: 'Home', href: '/' },
-    { displayName: 'About me', href: '/about' },
-    { displayName: 'Blog posts', href: '/blog' },
-    { displayName: 'Projects', href: '/projects' },
-  ] as const
+const links = [
+  { displayName: 'Home', href: '/' },
+  { displayName: 'My skills', href: '/skills' },
+  { displayName: 'About me', href: '/about' },
+  { displayName: 'Projects', href: '/projects' },
+] as const
+
+export default async function Sidebar() {
 
   return (
     <div
       id={id}
-      className={`fixed ${classes.menu} m-0 h-screen w-full sm:w-[24rem] flex-col items-stretch sm:border-r border-secondary border-opacity-25 bg-black p-0 text-neutral-100 [&::backdrop]:select-none [&::backdrop]:bg-secondary [&::backdrop]:bg-opacity-10 [&::backdrop]:backdrop-blur-sm [&:popover-open]:flex z-menu`}
+      className={`fixed ${classes.menu} m-0 h-full w-full sm:w-[24rem] flex-col items-stretch sm:border-r border-secondary border-opacity-25 bg-background p-0 text-text [&::backdrop]:select-none [&::backdrop]:bg-secondary [&::backdrop]:bg-opacity-10 [&::backdrop]:backdrop-blur-sm [&:popover-open]:flex z-menu`}
       popover='auto'
     >
       <div className={`${classes.background} absolute h-full w-full`}></div>
-      <div className='z-40 flex flex-col p-5'>
+      <div className='z-40 flex flex-grow flex-col p-5'>
         <div className='flex select-none items-center'>
           <div className='mask-icon aspect-square w-10 bg-gradient-to-r from-primary to-secondary'></div>
           <a href='/' className='ml-5 flex grow flex-col justify-between'>
@@ -28,7 +29,7 @@ export default function Sidebar() {
           </a>
           <IconButton icon='material-close' popoverTarget='sidebarMenu' />
         </div>
-        <div className='mt-8 flex flex-col gap-4 text-2xl font-bold *:select-none hover:*:bg-gradient-to-r hover:*:from-primary hover:*:to-secondary hover:*:bg-clip-text hover:*:text-transparent'>
+        <div className='mt-8 flex flex-grow flex-col gap-4 text-2xl font-bold *:select-none hover:*:bg-gradient-to-r hover:*:from-primary hover:*:to-secondary hover:*:bg-clip-text hover:*:text-transparent'>
           {links.map(({ displayName, href }) => (
             <a key={displayName} href={href}>
               {displayName}
