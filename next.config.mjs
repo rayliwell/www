7a756c216f80630a31c265b447207366163e7f77
Ryan Halliwell <git@rayliwell.com>
2026-03-22 11:51:39 +0000

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import remarkDirectiveRehype from 'remark-directive-rehype'
+import remarkDirective from 'remark-directive'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import remarkReadingTime from 'remark-reading-time'
+import readingMdxTime from 'remark-reading-time/mdx.js'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   webpack(config) {
+    // Required for css url tilde handling
+    config.resolve.modules.push('node_modules')
+
     return config
   },
 }
